e9a888d957f05d27feed64c422f2ba78209fa176
sadid56 <sadidhasan56@gmail.com>
2023-11-25 19:35:20 +0600

    user related add

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
-const app = express()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
 const port = process.env.PORT || 5600;
 
-
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dzbhwpo.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,93 +26,131 @@ async function run() {
     await client.connect();
 
     // collection
-    const featuredCollection = client.db('assignment-12').collection('featured')
-    const testomonialCollection = client.db('assignment-12').collection('testomonial')
-    const blogsCollection = client.db('assignment-12').collection('blog')
-    const trainersCollection = client.db('assignment-12').collection('trainer')
-    const newsLettersCollection = client.db('assignment-12').collection('newsLetters')
-    const galleryCollection = client.db('assignment-12').collection('gallery')
-    const classesCollection = client.db('assignment-12').collection('classes')
-    const communityCollection = client.db('assignment-12').collection('community')
+    const featuredCollection = client
+      .db("assignment-12")
+      .collection("featured");
+    const testomonialCollection = client
+      .db("assignment-12")
+      .collection("testomonial");
+    const blogsCollection = client.db("assignment-12").collection("blog");
+    const trainersCollection = client.db("assignment-12").collection("trainer");
+    const newsLettersCollection = client
+      .db("assignment-12")
+      .collection("newsLetters");
+    const galleryCollection = client.db("assignment-12").collection("gallery");
+    const classesCollection = client.db("assignment-12").collection("classes");
+    const communityCollection = client.db("assignment-12").collection("community");
+    const usersCollection = client.db("assignment-12").collection("users");
 
-    // CRUD OPERATION  
-    // featured related
-    app.get('/featured', async(req, res)=>{
-        const result = await featuredCollection.find().toArray()
-        res.send(result)
+    // CRUD OPERATION
+    //users related
+    app.post('/users', async(req, res)=>{
+      const user = req.body;
+      // console.log(user, user?.email);
+      const query = {email: user?.email}
+      const isExisting = await usersCollection.findOne(query)
+      if(isExisting){
+        return res.send({message: 'user already lodded', insertedId: null})
+      }
+      const result = await usersCollection.insertOne(user)
+      res.send(result)
     })
+
+    app.get('/users', async(req, res)=>{
+      const result = await usersCollection.find().toArray()
+      res.send(result)
+    })
+
+    // featured related
+    app.get("/featured", async (req, res) => {
+      const result = await featuredCollection.find().toArray();
+      res.send(result);
+    });
 
     //testomonial related
-    app.get('/testomonial', async(req, res)=>{
-        const result = await testomonialCollection.find().toArray()
-        res.send(result)
-    })
+    app.get("/testomonial", async (req, res) => {
+      const result = await testomonialCollection.find().toArray();
+      res.send(result);
+    });
 
     // blogs related
-    app.get('/blogs', async(req, res)=>{
-        const result = await blogsCollection.find().toArray()
-        res.send(result)
-    })
+    app.get("/blogs", async (req, res) => {
+      const result = await blogsCollection.find().toArray();
+      res.send(result);
+    });
 
     // trainer related
-    app.get('/trainers', async(req, res)=>{
-      const result = await trainersCollection.find().toArray()
-      res.send(result)
-    })
+    app.get("/trainers", async (req, res) => {
+      const result = await trainersCollection.find().toArray();
+      res.send(result);
+    });
 
     // letter related
-    app.post('/newsLetters', async(req, res)=>{
+    app.post("/newsLetters", async (req, res) => {
       const letter = req.body;
-      const result = await newsLettersCollection.insertOne(letter)
-      res.send(result)
-    })
+      const result = await newsLettersCollection.insertOne(letter);
+      res.send(result);
+    });
 
-    app.get('/newsLetters', async(req, res)=>{
-      const result = await newsLettersCollection.find().toArray()
-      res.send(result)
-    })
+    app.get("/newsLetters", async (req, res) => {
+      const result = await newsLettersCollection.find().toArray();
+      res.send(result);
+    });
 
     // gallery related
-    app.get('/gallery',async(req, res)=>{
-      const result = await galleryCollection.find().toArray()
-      res.send(result)
-    })
+    app.get("/gallery", async (req, res) => {
+      const result = await galleryCollection.find().toArray();
+      res.send(result);
+    });
 
     // classes related
-    app.post('/classes', async(req, res)=>{
-    const classes = req.body;
-    const result = await classesCollection.insertOne(classes)
-    res.send(result)
-    })
-
-    app.get('/classes', async(req, res)=>{
-      const result = await classesCollection.find().toArray()
-      res.send(result)
-    })
-
-    app.get('/classes/:id', async(req, res)=>{
-      const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await classesCollection.findOne(query)
+    app.post("/classes", async (req, res) => {
+      const classes = req.body;
+      const result = await classesCollection.insertOne(classes);
       res.send(result);
-    })
+    });
+
+    app.get("/classes", async (req, res) => {
+      const result = await classesCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/classes/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await classesCollection.findOne(query);
+      res.send(result);
+    });
 
     // community related
-    app.post('/community', async(req, res)=>{
+    app.post("/community", async (req, res) => {
       const post = req.body;
-      const result = await communityCollection.insertOne(post)
-      res.send(result)
-    })
+      const result = await communityCollection.insertOne(post);
+      res.send(result);
+    });
 
-    app.get('/community', async(req, res)=>{
-      const result = await communityCollection.find().toArray()
-      res.send(result)
-    })
+    app.get("/community", async (req, res) => {
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      console.log(page, size);
+      const result = await communityCollection
+        .find()
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+      res.send(result);
+    });
 
+    app.get("/pageCount", async (req, res) => {
+      const count = await communityCollection.estimatedDocumentCount();
+      res.send({ count });
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -122,11 +158,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Assignment 12 server running");
+});
 
-app.get('/', (req, res)=>{
-    res.send('Assignment 12 server running')
-})
-
-app.listen(port, ()=>{
-    console.log(`Server running ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server running ${port}`);
+});
