ca01f29236b4178883cfcdbd3a13d5a692d82679
sadid56 <sadidhasan56@gmail.com>
2023-11-29 11:18:57 +0600

    update it

--- index.js
+++ index.js
@@ -182,6 +182,11 @@ async function run() {
       const result = await bookedsCollection.insertOne(user)
       res.send(result)
     })
+
+    app.get('/bookeds', async(req, res)=>{
+      const result = await bookedsCollection.find().toArray()
+      res.send(result)
+    })
     
 
     // featured related
